fa202d5438539160c9d1c38086dbbd81fd35d5d0
joelj <jjayoma2017@gmail.com>
2024-02-03 19:11:26 +0800

    Initial commit. Added javafx lifeCycle.

---
+++ src/main/java/com/example/firstapp/HelloApplication.java
@@ -0,0 +1,40 @@
+package com.example.firstapp;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void init() throws InterruptedException {
+        System.out.println("Before the app starts");
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(5000);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(Thread.currentThread().getName());
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20,20,20,20));
+        root.setSpacing(20);
+        Label text = new Label("");
+        Button button = new Button("Click Me!");
+        button.setOnAction(e -> text.setText("Welcome to JavaFX Application!"));
+        root.getChildren().addAll(text,button);
+        Scene scene = new Scene(root, 350,300);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Application");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(); // 2 Threads 1. Java FX launcher 2. JavaFX application thread
+    }
+}
